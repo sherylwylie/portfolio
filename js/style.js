f42bb5bdcee992d574d2e17186c024ba8f6729c2
sherylwylie <67860596+sherylwylie@users.noreply.github.com>
2020-07-05 19:53:42 +0100

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -10,3 +10,25 @@ function closeNav() {
   $('#menu').show();
   $('#menu2').show();
 }
+
+$( document ).ready(function() {
+  $(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
+});
